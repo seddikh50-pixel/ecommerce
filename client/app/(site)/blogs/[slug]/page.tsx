1cc78feba16165c3a6e090d3a0caf5f5266dad9f
اسمك هنا <@example.com>
2026-01-25 22:41:35 +0100

    promise

--- client/app/(site)/blogs/[slug]/page.tsx
+++ client/app/(site)/blogs/[slug]/page.tsx
@@ -4,33 +4,70 @@ import { getAllBlogs } from '@/lib/cache';
 import Image from 'next/image';
 import React from 'react'
 
+// interface BlogId {
+//   params: { slug: string }
+
+// }
+
+// const page = async ({ params }: BlogId) => {
+//   const { slug } = await params
+
+//   const blogName = decodeURIComponent(slug);
+
+//   const blog = (await getAllBlogs()).find((bl) => bl.title.toLowerCase().trim() === blogName.toLowerCase())
+  
+//    if (!blog) {
+//     return "no blog"
+//   }
+
+//   return (
+//     <Container className='flex flex-col space-y-5' >
+//       <Pathname productTitle={blogName}/>
+
+//       <div className='relative w-[700px] h-[400px]'>
+//         <Image src={blog.image} fill alt='' />
+//       </div>
+//       <p className='text-2xl font-black'> {blog.title} </p>
+//        <h1 className='max-w-[1000px] whitespace-pre-line text-xl text-gray-700 '>{blog.content}</h1>
+//        <p> </p>
+      
+//     </Container>
+//   )
+// }
+
+// export default page
+
+
+
 interface BlogId {
   params: { slug: string }
-
 }
 
 const page = async ({ params }: BlogId) => {
-  const { slug } = await params
+  const { slug } = params   // ✅ بدون await
 
   const blogName = decodeURIComponent(slug);
 
-  const blog = (await getAllBlogs()).find((bl) => bl.title.toLowerCase().trim() === blogName.toLowerCase())
-  
-   if (!blog) {
-    return "no blog"
+  const blog = (await getAllBlogs()).find(
+    (bl) => bl.title.toLowerCase().trim() === blogName.toLowerCase()
+  )
+
+  if (!blog) {
+    return <div>no blog</div>
   }
 
   return (
-    <Container className='flex flex-col space-y-5' >
-      <Pathname productTitle={blogName}/>
+    <Container className='flex flex-col space-y-5'>
+      <Pathname productTitle={blogName} />
 
       <div className='relative w-[700px] h-[400px]'>
         <Image src={blog.image} fill alt='' />
       </div>
-      <p className='text-2xl font-black'> {blog.title} </p>
-       <h1 className='max-w-[1000px] whitespace-pre-line text-xl text-gray-700 '>{blog.content}</h1>
-       <p> </p>
-      
+
+      <p className='text-2xl font-black'>{blog.title}</p>
+      <h1 className='max-w-[1000px] whitespace-pre-line text-xl text-gray-700'>
+        {blog.content}
+      </h1>
     </Container>
   )
 }
