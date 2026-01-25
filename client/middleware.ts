c8170e18df1856a1882c3c30e967f2b42577e4df
اسمك هنا <@example.com>
2026-01-25 22:15:15 +0100

    middleware edited

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,52 +1,45 @@
 import { NextResponse } from "next/server";
-
 import { NextRequest } from "next/server";
-import authMiddleware from "./middlewares/authMiddleware";
-import logMiddleware from "./middlewares/logMiddleware";
-import adminMiddleware from "./middlewares/adminMiddleware";
 import { cookies } from "next/headers";
 import authAdmin from "./middlewares/adminMiddleware";
 import { userMiddleware } from "./middlewares/userMiddleware";
-import { strict } from "assert";
-
-
-
 
 const middleware = async (request: NextRequest) => {
    const pathname = request.nextUrl.pathname
    const cookieStore = await cookies();
    const userCookie = cookieStore.get('user')?.value
+
+   // حماية cart و success
    if (pathname.startsWith('/cart') || pathname.startsWith('/success')) {
       if (!userCookie) {
          return NextResponse.redirect(new URL("/account", request.url));
       }
       const successToken = await userMiddleware(userCookie)
-      if (!successToken.success) return NextResponse.redirect(new URL("/account", request.url));
+      if (!successToken.success) {
+         return NextResponse.redirect(new URL("/account", request.url));
+      }
       return NextResponse.next();
    }
 
+   // حماية admin
    if (pathname.startsWith('/admin')) {
       const token = cookieStore.get('token')?.value
       if (!token) {
          return NextResponse.redirect(new URL("/login", request.url));
       }
       const jwtVerify = await authAdmin(token)
-      
       if (!jwtVerify) {
          return NextResponse.redirect(new URL("/login", request.url));
-
       }
-       return NextResponse.next()
+      return NextResponse.next()
    }
 
-
+   // ✅ مهم جدًا: السماح لباقي المسارات
+   return NextResponse.next();
 }
 
 export default middleware;
-// export const config = {
-//    matcher: ['/admin/:path*', '/api/:path*'],
-// }
 
 export const config = {
-   matcher: ["/cart", '/api/:path*', '/admin/:path*', "/success"],
+   matcher: ["/cart", "/success", "/admin/:path*", "/api/:path*"],
 };
