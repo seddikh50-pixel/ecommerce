7bf6667f1062e7f7ae0bbd32e8d915770d0761c7
اسمك هنا <@example.com>
2026-01-25 22:46:12 +0100

    another promise

--- client/app/(site)/blogs/[slug]/page.tsx
+++ client/app/(site)/blogs/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import Container from '@/components/common/Container';
-import Pathname from '@/components/pathname/Pathname';
-import { getAllBlogs } from '@/lib/cache';
-import Image from 'next/image';
-import React from 'react'
+// import Container from '@/components/common/Container';
+// import Pathname from '@/components/pathname/Pathname';
+// import { getAllBlogs } from '@/lib/cache';
+// import Image from 'next/image';
+// import React from 'react'
 
 // interface BlogId {
 //   params: { slug: string }
@@ -38,20 +38,27 @@ import React from 'react'
 // export default page
 
 
+import Container from '@/components/common/Container';
+import Pathname from '@/components/pathname/Pathname';
+import { getAllBlogs } from '@/lib/cache';
+import Image from 'next/image';
+import React from 'react'
 
+/* ✅ النوع الصحيح حسب Next 15 */
 interface BlogId {
-  params: { slug: string }
+  params: Promise<{ slug: string }>
 }
 
 const page = async ({ params }: BlogId) => {
-  const { slug } = params   // ✅ بدون await
+  /* ✅ await لأن params Promise */
+  const { slug } = await params
 
   const blogName = decodeURIComponent(slug);
 
   const blog = (await getAllBlogs()).find(
     (bl) => bl.title.toLowerCase().trim() === blogName.toLowerCase()
   )
-
+  
   if (!blog) {
     return <div>no blog</div>
   }
@@ -65,6 +72,7 @@ const page = async ({ params }: BlogId) => {
       </div>
 
       <p className='text-2xl font-black'>{blog.title}</p>
+
       <h1 className='max-w-[1000px] whitespace-pre-line text-xl text-gray-700'>
         {blog.content}
       </h1>
