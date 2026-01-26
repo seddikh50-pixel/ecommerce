deed0b6d71e2c2ec542551a9deafc7de5125144e
اسمك هنا <@example.com>
2026-01-26 20:29:18 +0100

    admin lyaout

--- client/app/admin/layout.tsx
+++ client/app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, Suspense } from "react";
 import SideBar from "@/components/admin/SideBar";
+
 import Admin from "@/components/admin/Admin";
 
 
@@ -8,25 +9,33 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  return <>
-    <div className="">
-      <div className="h-14  w-full px-10   bg-white border-b border-gray-600">
-         <Suspense fallback={null}>
-          <Admin />
-         </Suspense>
-      </div>
-      <div className="flex" >
-        <div className="xl:w-64 lg:w-64 w-48   ">
+  return (
+    <Suspense fallback={null}>
+      <div>
+        {/* Top Bar */}
+        <div className="h-14 w-full px-10 bg-white border-b border-gray-600">
           <Suspense fallback={null}>
-            <SideBar />
+            <Admin />
           </Suspense>
         </div>
-        <div className="flex-1">
-          {children}
+
+        {/* Body */}
+        <div className="flex">
+          {/* Sidebar */}
+          <div className="xl:w-64 lg:w-64 w-48">
+            <Suspense fallback={null}>
+              <SideBar />
+            </Suspense>
+          </div>
+
+          {/* Content */}
+          <div className="flex-1">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  </>;
+    </Suspense>
+  );
 };
 
 export default Layout;
