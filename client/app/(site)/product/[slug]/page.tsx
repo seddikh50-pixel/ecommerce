03114b95156ec811f9cead7adcc7fa127ef20375
اسمك هنا <@example.com>
2026-01-27 12:55:51 +0100

    cache life

--- client/app/(site)/product/[slug]/page.tsx
+++ client/app/(site)/product/[slug]/page.tsx
@@ -8,15 +8,18 @@ import Container from '@/components/common/Container';
 import ImageView from '@/app/pages/singleProduct/ImageView';
 import ShareBadge from '../../../pages/singleProduct/ShareBadge'
 import DetailsView from '@/app/pages/singleProduct/DetailsView';
+import { cacheLife } from 'next/cache';
 
 // import ProductCharastiristics from '@/components/common/ProductCharastiristics'
 
 interface Params {
- params: Promise<{ slug: string }>
+  params: Promise<{ slug: string }>
 }
 
 const SingleProduct = async ({ params }: Params) => {
-  
+  'use cache'
+  cacheLife('seconds') // 
+
   const { slug } = await params
   const productName = decodeURIComponent(slug);
   const product = (await getAllProducts()).filter((pro) => { return pro.name === productName })[0]
@@ -34,7 +37,7 @@ const SingleProduct = async ({ params }: Params) => {
           <div className=' w-full md:w-2/5'>
             <ImageView images={product?.images} />
           </div>
-          <DetailsView product={product}/>
+          <DetailsView product={product} />
         </div>
       </Container>
     </div>
