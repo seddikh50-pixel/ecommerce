3ab057c062700b65f536d4390a558b81ccba5dd3
اسمك هنا <@example.com>
2026-01-27 18:16:04 +0100

    back to origin

--- client/components/header/SearchProducts.tsx
+++ client/components/header/SearchProducts.tsx
@@ -60,13 +60,7 @@ const SearchProducts = ({ products }: ListProducts) => {
 
 
     return (
-        <div className={`absolute  w-full shadow-sm bg-gray-100 h-64  z-40 left-0 lg:hidden xl:hidden transition-all duration-300 top-20 p-4  translate-y-0 opacity-100  `}>
-            <div>
-                {products.map((product) => (    
-                    <h1 key={product.id}>{product.name}</h1>
-                ))
-                            }
-            </div>
+        <div className={`absolute  w-full shadow-sm bg-gray-100 h-64  z-40 left-0 lg:hidden xl:hidden transition-all duration-300 top-20 p-4 ${showSearch ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0 pointer-events-none"} `}>
             <form onSubmit={(e) => e.preventDefault()} action="" className='relative   '>
                 <Input placeholder='Search...' className='flex-1 focus-visible:ring-0
                      focus-visible:border-store placeholder:tracking-wider text-black
@@ -79,7 +73,7 @@ const SearchProducts = ({ products }: ListProducts) => {
             </form>
             <div className='absolute w-[95%] left-3 mt-2  z-4'>
                 <h1 className='p-2 text-black border rounded-t-sm bg-gray-100  '>Popular Products </h1>
-                <div className='bg-yellow-200    '>
+                <div className='    '>
 
                     {loading ?
                         <div className="text-black text-sm flex justify-center items-center gap-2 py-4">
